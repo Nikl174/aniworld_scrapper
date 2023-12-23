8d04c794ccfd30cef1da1fee7fb7ccec91559e10
nikl174 <niclasst81@gmail.com>
2023-12-24 00:06:35 +0100

    first commit

---
+++ scrape_aniworld.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+import requests
+import sys
+import bs4
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+
+ARGS = sys.argv
+URL = ""
+
+if len(ARGS) > 1:
+    URL = ARGS[1]
+else:
+    print("Usage: ", ARGS[0], " ANIME_URL")
+    print("NOTE: URL without seasons or episodes")
+    exit(-1)
+has_movie = False
+
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+seasons = int(soup.find("meta", itemprop="numberOfSeasons").get("content"))
+if seasons >= 1:
+    if soup.find("a", title="Alle Filme") != None:
+        seasons -= 1
+        has_movie = True
+
+prompt = str("Enter Season[1-" + str(seasons) + "]:\n")
+season: int = input(prompt)
+
+page_season = requests.get(URL + "staffel-" + str(season))
+soup = BeautifulSoup(page_season.content, "html.parser")
+
+episodes: bs4.element.ResultSet = soup.find_all("tr", itemprop="episode")
+links = dict()
+e_max = 0
+for episode in episodes:
+    e_id = int(episode.get("data-episode-season-id"))
+    links[e_id] = episode.find("a", itemprop="url")["href"]
+    e_max = max(e_max, e_id)
+
+prompt = str("Enter Episode to start from [1-" + str(e_max) + "]:\n")
+start_episode = input(prompt)
+
+for link_id in links:
+    if int(link_id) >= int(start_episode):
+        new_url = urljoin(URL, str(links[link_id]))
+        episode_page = requests.get(new_url)
+        soup = BeautifulSoup(episode_page.content, "html.parser")
+        next_link = soup.find("i", title="Hoster Vidoza").find_parent(
+            "a", {"class": "watchEpisode"})["href"]
+        real_page = requests.get(urljoin(URL, str(next_link)))
+        soup = BeautifulSoup(real_page.content, "html.parser")
+        real_link = soup.find("video", id="player").find("source")
+        print(real_link["src"])
+
+# prompt = str("Enter Episode[1-" + str(e_max) + "] (0 for all):\n")
+# episode: int = input(prompt)
+
+# print(episodes)
